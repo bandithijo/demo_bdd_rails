db581ba1219d0d5f9ed4f88ecfea260f5706ef16
Rizqi Nur Assyaufi <bandithijo@gmail.com>
2021-05-31 06:07:42 +0800

    reposition database_cleaner on spec/rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,6 +60,17 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
 
 Shoulda::Matchers.configure do |config|
@@ -70,18 +81,5 @@ Shoulda::Matchers.configure do |config|
     with.library :active_model
     with.library :action_controller
     with.library :rails
-  end
-end
-
-RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
-  config.around(:each) do |example|
-    DatabaseCleaner.cleaning do
-      example.run
-    end
   end
 end
